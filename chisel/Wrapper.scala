81fc4e76084a5bf0bf50faa917410ed838d0897f
Seungwoo Lee <lsw8075@gmail.com>
2023-06-15 17:54:08 +0900

    interfacing AXI master/slave port

--- chisel/Wrapper.scala
+++ chisel/Wrapper.scala
@@ -25,44 +25,83 @@ class WrapperIO extends Bundle
   // interrupts
   val interrupt = Input(Bool())
 
+  // M.AXI.AR
+  val m_axi_araddr = Output(UInt(32.W))
+  val m_axi_arburst = Output(UInt(2.W))
+  val m_axi_arcache = Output(UInt(4.W))
+  val m_axi_arlen = Output(UInt(8.W))
+  val m_axi_arprot = Output(UInt(3.W))
+  val m_axi_arsize = Output(UInt(3.W))
+  val m_axi_arvalid = Output(Bool())
+  val m_axi_arready = Input(Bool())
+
+  // M.AXI.R
+  val m_axi_rdata = Input(UInt(32.W))
+  val m_axi_rlast = Input(Bool())
+  val m_axi_rresp = Input(UInt(2.W))
+  val m_axi_rvalid = Input(Bool())
+  val m_axi_rready = Output(Bool())
+
+  // M.AXI.AW
+  val m_axi_awaddr = Output(UInt(32.W))
+  val m_axi_awburst = Output(UInt(2.W))
+  val m_axi_awcache = Output(UInt(4.W))
+  val m_axi_awlen = Output(UInt(8.W))
+  val m_axi_awprot = Output(UInt(3.W))
+  val m_axi_awsize = Output(UInt(3.W))
+  val m_axi_awvalid = Output(Bool())
+  val m_axi_awready = Input(Bool())
+
+  // M.AXI.B
+  val m_axi_bready = Output(Bool())
+  val m_axi_bresp = Input(UInt(2.W))
+  val m_axi_bvalid = Input(Bool())
+
+  // M.AXI.W
+  val m_axi_wdata = Output(UInt(32.W))
+  val m_axi_wlast = Output(Bool())
+  val m_axi_wstrb = Output(UInt(4.W))
+  val m_axi_wvalid = Output(Bool())
+  val m_axi_wready = Input(Bool())
+
   // S.AXI.AR
-  val s_axi_araddr = Output(UInt(32.W))
-  val s_axi_arburst = Output(UInt(2.W))
-  val s_axi_arcache = Output(UInt(4.W))
-  val s_axi_arlen = Output(UInt(8.W))
-  val s_axi_arprot = Output(UInt(3.W))
-  val s_axi_arsize = Output(UInt(3.W))
-  val s_axi_arvalid = Output(Bool())
-  val s_axi_arready = Input(Bool())
+  val s_axi_araddr = Input(UInt(32.W))
+  val s_axi_arburst = Input(UInt(2.W))
+  val s_axi_arcache = Input(UInt(4.W))
+  val s_axi_arlen = Input(UInt(8.W))
+  val s_axi_arprot = Input(UInt(3.W))
+  val s_axi_arsize = Input(UInt(3.W))
+  val s_axi_arvalid = Input(Bool())
+  val s_axi_arready = Output(Bool())
 
   // S.AXI.R
-  val s_axi_rdata = Input(UInt(32.W))
-  val s_axi_rlast = Input(Bool())
-  val s_axi_rresp = Input(UInt(2.W))
-  val s_axi_rvalid = Input(Bool())
-  val s_axi_rready = Output(Bool())
+  val s_axi_rdata = Output(UInt(32.W))
+  val s_axi_rlast = Output(Bool())
+  val s_axi_rresp = Output(UInt(2.W))
+  val s_axi_rvalid = Output(Bool())
+  val s_axi_rready = Input(Bool())
 
   // S.AXI.AW
-  val s_axi_awaddr = Output(UInt(32.W))
-  val s_axi_awburst = Output(UInt(2.W))
-  val s_axi_awcache = Output(UInt(4.W))
-  val s_axi_awlen = Output(UInt(8.W))
-  val s_axi_awprot = Output(UInt(3.W))
-  val s_axi_awsize = Output(UInt(3.W))
-  val s_axi_awvalid = Output(Bool())
-  val s_axi_awready = Input(Bool())
+  val s_axi_awaddr = Input(UInt(32.W))
+  val s_axi_awburst = Input(UInt(2.W))
+  val s_axi_awcache = Input(UInt(4.W))
+  val s_axi_awlen = Input(UInt(8.W))
+  val s_axi_awprot = Input(UInt(3.W))
+  val s_axi_awsize = Input(UInt(3.W))
+  val s_axi_awvalid = Input(Bool())
+  val s_axi_awready = Output(Bool())
 
   // S.AXI.B
-  val s_axi_bready = Output(Bool())
-  val s_axi_bresp = Input(UInt(2.W))
-  val s_axi_bvalid = Input(Bool())
+  val s_axi_bready = Input(Bool())
+  val s_axi_bresp = Output(UInt(2.W))
+  val s_axi_bvalid = Output(Bool())
 
   // S.AXI.W
-  val s_axi_wdata = Output(UInt(32.W))
-  val s_axi_wlast = Output(Bool())
-  val s_axi_wstrb = Output(UInt(4.W))
-  val s_axi_wvalid = Output(Bool())
-  val s_axi_wready = Input(Bool()) 
+  val s_axi_wdata = Input(UInt(32.W))
+  val s_axi_wlast = Input(Bool())
+  val s_axi_wstrb = Input(UInt(4.W))
+  val s_axi_wvalid = Input(Bool())
+  val s_axi_wready = Output(Bool())
 }
 
 class WrapperPeripheral(implicit p: Parameters) extends LazyModule 
@@ -72,7 +111,7 @@ class WrapperPeripheral(implicit p: Parameters) extends LazyModule
   val slavenode = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
     slaves = Seq(AXI4SlaveParameters(
       address       = List(AddressSet(0x1000, 0x3ff)),
-      resources     = device.reg("slave"),
+      resources     = device.reg("mmio-tsn"),
       regionType    = RegionType.UNCACHED,
       supportsWrite = TransferSizes(1, 4),
       supportsRead  = TransferSizes(1, 4),
@@ -81,16 +120,33 @@ class WrapperPeripheral(implicit p: Parameters) extends LazyModule
 
   val intnode = IntSourceNode(IntSourcePortSimple(num = 1, resources = device.int))
 
-  val tl_slave = TLIdentityNode()
+  val masternode = AXI4MasterNode(Seq(AXI4MasterPortParameters(
+    masters  = Seq(AXI4MasterParameters(
+      name = "dma-tsn"
+    ))
+  )))
+
+  val tl_manager = TLIdentityNode()
     (slavenode :=
         AXI4Buffer() :=
         AXI4Fragmenter() :=
         AXI4UserYanker(Some(16)) :=
         AXI4Deinterleaver(64) :=
         TLToAXI4() :=
-        TLFragmenter(4, 64, holdFirstDeny = true)
-      := tl_slave)
+        TLFragmenter(4, 64, holdFirstDeny = true) :=
+        TLBuffer() :=
+      tl_manager)
 
+  val tl_client =  TLIdentityNode()
+    (tl_client :=
+      TLBuffer() :=
+      AXI4ToTL() :=
+      AXI4UserYanker(Some(16)) :=
+      AXI4Fragmenter() :=
+      AXI4Buffer() :=
+    masternode)
+
+  
   lazy val module = new Impl
   class Impl extends LazyModuleImp(this) {
 
@@ -98,6 +154,7 @@ class WrapperPeripheral(implicit p: Parameters) extends LazyModule
 
     val (s, _) = slavenode.in(0)
     val (i, _) = intnode.out(0)
+    val (m, _) = masternode.out(0)
 
     // control
     io.clock_out      := clock 
@@ -106,45 +163,85 @@ class WrapperPeripheral(implicit p: Parameters) extends LazyModule
     // interrupt
     i(0)              := io.interrupt
 
+    // M.AXI.AR
+    s.ar.ready        := io.m_axi_arready
+    io.m_axi_arvalid  := s.ar.valid
+    io.m_axi_araddr   := s.ar.bits.addr
+    io.m_axi_arburst  := s.ar.bits.burst
+    io.m_axi_arcache  := s.ar.bits.cache
+    io.m_axi_arlen    := s.ar.bits.len
+    io.m_axi_arprot   := s.ar.bits.prot 
+    io.m_axi_arsize   := s.ar.bits.size
+
+    // M.AXI.R
+    io.m_axi_rready   := s.r.ready
+    s.r.valid         := io.m_axi_rvalid
+    s.r.bits.data     := io.m_axi_rdata
+    s.r.bits.last     := io.m_axi_rlast
+    s.r.bits.resp     := io.m_axi_rresp
+
+    // M.AXI.AW
+    s.aw.ready        := io.m_axi_awready
+    io.m_axi_awvalid  := s.aw.valid
+    io.m_axi_awaddr   := s.aw.bits.addr
+    io.m_axi_awburst  := s.aw.bits.burst
+    io.m_axi_awcache  := s.aw.bits.cache
+    io.m_axi_awlen    := s.aw.bits.len
+    io.m_axi_awprot   := s.aw.bits.prot
+    io.m_axi_awsize   := s.aw.bits.size
+
+    // M.AXI.B
+    io.m_axi_bready   := s.b.ready
+    s.b.valid         := io.m_axi_bvalid
+    s.b.bits.resp     := io.m_axi_bresp
+
+    // M.AXI.W
+    s.w.ready         := io.m_axi_wready
+    io.m_axi_wvalid   := s.w.valid
+    io.m_axi_wdata    := s.w.bits.data
+    io.m_axi_wlast    := s.w.bits.last
+    io.m_axi_wstrb    := s.w.bits.strb
+
     // S.AXI.AR
-    s.ar.ready        := io.s_axi_arready 
-    io.s_axi_arvalid  := s.ar.valid
-    io.s_axi_araddr   := s.ar.bits.addr
-    io.s_axi_arburst  := s.ar.bits.burst
-    io.s_axi_arcache  := s.ar.bits.cache
-    io.s_axi_arlen    := s.ar.bits.len
-    io.s_axi_arprot   := s.ar.bits.prot 
-    io.s_axi_arsize   := s.ar.bits.size
+    io.s_axi_arready  := m.ar.ready
+    m.ar.valid        := io.s_axi_arvalid
+    m.ar.bits.addr    := io.s_axi_araddr
+    m.ar.bits.burst   := io.s_axi_arburst
+    m.ar.bits.cache   := io.s_axi_arcache
+    m.ar.bits.len     := io.s_axi_arlen
+    m.ar.bits.prot    := io.s_axi_arprot
+    m.ar.bits.size    := io.s_axi_arsize
 
     // S.AXI.R
-    io.s_axi_rready   := s.r.ready
-    s.r.valid         := io.s_axi_rvalid
-    s.r.bits.data     := io.s_axi_rdata
-    s.r.bits.last     := io.s_axi_rlast
-    s.r.bits.resp     := io.s_axi_rresp
-
+    m.r.ready         := io.s_axi_rready
+    io.s_axi_rvalid   := m.r.valid
+    io.s_axi_rdata    := m.r.bits.data
+    io.s_axi_rlast    := m.r.bits.last
+    io.s_axi_rresp    := m.r.bits.resp
+    
     // S.AXI.AW
-    s.aw.ready        := io.s_axi_awready
-    io.s_axi_awvalid  := s.aw.valid
-    io.s_axi_awaddr   := s.aw.bits.addr
-    io.s_axi_awburst  := s.aw.bits.burst
-    io.s_axi_awcache  := s.aw.bits.cache
-    io.s_axi_awlen    := s.aw.bits.len
-    io.s_axi_awprot   := s.aw.bits.prot
-    io.s_axi_awsize   := s.aw.bits.size
-
+    io.s_axi_awready  := m.aw.ready
+    m.aw.valid        := io.s_axi_awvalid
+    m.aw.bits.addr    := io.s_axi_awaddr
+    m.aw.bits.burst   := io.s_axi_awburst
+    m.aw.bits.cache   := io.s_axi_awcache
+    m.aw.bits.len     := io.s_axi_awlen
+    m.aw.bits.prot    := io.s_axi_awprot
+    m.aw.bits.size    := io.s_axi_awsize
+    
     // S.AXI.B
-    io.s_axi_bready   := s.b.ready
-    s.b.valid         := io.s_axi_bvalid
-    s.b.bits.resp     := io.s_axi_bresp
+    m.b.ready         := io.s_axi_bready
+    io.s_axi_bvalid   := m.b.valid
+    io.s_axi_bresp    := m.b.bits.resp
 
     // S.AXI.W
-    s.w.ready         := io.s_axi_wready
-    io.s_axi_wvalid   := s.w.valid
-    io.s_axi_wdata    := s.w.bits.data
-    io.s_axi_wlast    := s.w.bits.last
-    io.s_axi_wstrb    := s.w.bits.strb
+    io.s_axi_wready   := m.w.ready
+    m.w.valid         := io.s_axi_wvalid
+    m.w.bits.data     := io.s_axi_wdata
+    m.w.bits.last     := io.s_axi_wlast
+    m.w.bits.strb     := io.s_axi_wstrb
   }
+
 }
 
 trait HasWrapper { this: BaseSubsystem =>
@@ -153,8 +250,11 @@ trait HasWrapper { this: BaseSubsystem =>
 
   ibus.fromSync := wrapper.intnode
   pbus.toFixedWidthSlave(Some("mmio-tsn")) {
-      (wrapper.tl_slave)
+      (wrapper.tl_manager)
   }
+  fbus.fromMaster(Some("dma-tsn"), buffer = BufferParams.default) {
+    TLBuffer.chainNode(0)
+  } := (wrapper.tl_client)
 }
 
 trait HasWrapperImp extends LazyModuleImp {
@@ -163,3 +263,4 @@ trait HasWrapperImp extends LazyModuleImp {
   wrapperio <> outer.wrapper.module.io
   dontTouch(wrapperio)
 }
+
--- vsrc/RocketTestTop.v
+++ vsrc/RocketTestTop.v
@@ -42,8 +42,40 @@ module RocketTestTop(
         output wire [1:0] ddr3_dm,
         output wire ddr3_odt
     );
-/*
+
     wire        wrapperio_interrupt;
+    wire        wrapperio_clock_out;
+    wire        wrapperio_reset_out;
+    wire        wrapperio_m_axi_arready;
+    wire [31:0] wrapperio_m_axi_rdata;
+    wire        wrapperio_m_axi_rlast;
+    wire [1:0]  wrapperio_m_axi_rresp;
+    wire        wrapperio_m_axi_rvalid;
+    wire        wrapperio_m_axi_awready;
+    wire [1:0]  wrapperio_m_axi_bresp;
+    wire        wrapperio_m_axi_bvalid;
+    wire        wrapperio_m_axi_wready;
+    wire [31:0] wrapperio_m_axi_araddr;
+    wire [1:0]  wrapperio_m_axi_arburst;
+    wire [3:0]  wrapperio_m_axi_arcache;
+    wire [7:0]  wrapperio_m_axi_arlen;
+    wire [2:0]  wrapperio_m_axi_arprot;
+    wire        wrapperio_m_axi_arsize;
+    wire        wrapperio_m_axi_arvalid;
+    wire        wrapperio_m_axi_rready;
+    wire [31:0] wrapperio_m_axi_awaddr;
+    wire [1:0]  wrapperio_m_axi_awburst;
+    wire [3:0]  wrapperio_m_axi_awcache;
+    wire [7:0]  wrapperio_m_axi_awlen;
+    wire [2:0]  wrapperio_m_axi_awprot;
+    wire        wrapperio_m_axi_awsize;
+    wire        wrapperio_m_axi_awvalid;
+    wire        wrapperio_m_axi_bready;
+    wire [31:0] wrapperio_m_axi_wdata;
+    wire        wrapperio_m_axi_wlast;
+    wire [3:0]  wrapperio_m_axi_wstrb;
+    wire        wrapperio_m_axi_wvalid;
+   
     wire        wrapperio_s_axi_arready;
     wire [31:0] wrapperio_s_axi_rdata;
     wire        wrapperio_s_axi_rlast;
@@ -53,8 +85,6 @@ module RocketTestTop(
     wire [1:0]  wrapperio_s_axi_bresp;
     wire        wrapperio_s_axi_bvalid;
     wire        wrapperio_s_axi_wready;
-    wire        wrapperio_clock_out;
-    wire        wrapperio_reset_out;
     wire [31:0] wrapperio_s_axi_araddr;
     wire [1:0]  wrapperio_s_axi_arburst;
     wire [3:0]  wrapperio_s_axi_arcache;
@@ -75,7 +105,15 @@ module RocketTestTop(
     wire        wrapperio_s_axi_wlast;
     wire [3:0]  wrapperio_s_axi_wstrb;
     wire        wrapperio_s_axi_wvalid;
-*/
+    
+    wire bram_rst;
+    wire bram_clk;
+    wire bram_en;
+    wire [3:0] bram_we;
+    wire [11:0] bram_addr;
+    wire [31:0] bram_wrdata;
+    wire [31:0] bram_rddata;
+
     Arty100THarness rocketchip(
         .sys_clock(clock),
         .reset(reset),
@@ -95,9 +133,40 @@ module RocketTestTop(
         .ddr_ddr3_cke(ddr3_cke),
         .ddr_ddr3_cs_n(ddr3_cs_n),
         .ddr_ddr3_dm(ddr3_dm),
-        .ddr_ddr3_odt(ddr3_odt) /*,
+        .ddr_ddr3_odt(ddr3_odt),
         
     .wrapperio_interrupt       (wrapperio_interrupt),
+    .wrapperio_clock_out       (wrapperio_clock_out),
+    .wrapperio_reset_out       (wrapperio_reset_out),
+    .wrapperio_m_axi_arready   (wrapperio_m_axi_arready),
+    .wrapperio_m_axi_rdata     (wrapperio_m_axi_rdata),
+    .wrapperio_m_axi_rlast     (wrapperio_m_axi_rlast),
+    .wrapperio_m_axi_rresp     (wrapperio_m_axi_rresp),
+    .wrapperio_m_axi_rvalid    (wrapperio_m_axi_rvalid),
+    .wrapperio_m_axi_awready   (wrapperio_m_axi_awready),
+    .wrapperio_m_axi_bresp     (wrapperio_m_axi_bresp),
+    .wrapperio_m_axi_bvalid    (wrapperio_m_axi_bvalid),
+    .wrapperio_m_axi_wready    (wrapperio_m_axi_wready),
+    .wrapperio_m_axi_araddr    (wrapperio_m_axi_araddr),
+    .wrapperio_m_axi_arburst   (wrapperio_m_axi_arburst),
+    .wrapperio_m_axi_arcache   (wrapperio_m_axi_arcache),
+    .wrapperio_m_axi_arlen     (wrapperio_m_axi_arlen),
+    .wrapperio_m_axi_arprot    (wrapperio_m_axi_arprot),
+    .wrapperio_m_axi_arsize    (wrapperio_m_axi_arsize),
+    .wrapperio_m_axi_arvalid   (wrapperio_m_axi_arvalid),
+    .wrapperio_m_axi_rready    (wrapperio_m_axi_rready),
+    .wrapperio_m_axi_awaddr    (wrapperio_m_axi_awaddr),
+    .wrapperio_m_axi_awburst   (wrapperio_m_axi_awburst),
+    .wrapperio_m_axi_awcache   (wrapperio_m_axi_awcache),
+    .wrapperio_m_axi_awlen     (wrapperio_m_axi_awlen),
+    .wrapperio_m_axi_awprot    (wrapperio_m_axi_awprot),
+    .wrapperio_m_axi_awsize    (wrapperio_m_axi_awsize),
+    .wrapperio_m_axi_awvalid   (wrapperio_m_axi_awvalid),
+    .wrapperio_m_axi_bready    (wrapperio_m_axi_bready),
+    .wrapperio_m_axi_wdata     (wrapperio_m_axi_wdata),
+    .wrapperio_m_axi_wlast     (wrapperio_m_axi_wlast),
+    .wrapperio_m_axi_wstrb     (wrapperio_m_axi_wstrb),
+    .wrapperio_m_axi_wvalid    (wrapperio_m_axi_wvalid),
     .wrapperio_s_axi_arready   (wrapperio_s_axi_arready),
     .wrapperio_s_axi_rdata     (wrapperio_s_axi_rdata),
     .wrapperio_s_axi_rlast     (wrapperio_s_axi_rlast),
@@ -107,8 +176,6 @@ module RocketTestTop(
     .wrapperio_s_axi_bresp     (wrapperio_s_axi_bresp),
     .wrapperio_s_axi_bvalid    (wrapperio_s_axi_bvalid),
     .wrapperio_s_axi_wready    (wrapperio_s_axi_wready),
-    .wrapperio_clock_out       (wrapperio_clock_out),
-    .wrapperio_reset_out       (wrapperio_reset_out),
     .wrapperio_s_axi_araddr    (wrapperio_s_axi_araddr),
     .wrapperio_s_axi_arburst   (wrapperio_s_axi_arburst),
     .wrapperio_s_axi_arcache   (wrapperio_s_axi_arcache),
@@ -129,8 +196,62 @@ module RocketTestTop(
     .wrapperio_s_axi_wlast     (wrapperio_s_axi_wlast),
     .wrapperio_s_axi_wstrb     (wrapperio_s_axi_wstrb),
     .wrapperio_s_axi_wvalid    (wrapperio_s_axi_wvalid)
-    */
+    
+    
     );
+    
+    axi_bram_ctrl_0 bram_ctrl(
+    .s_axi_aclk      (wrapperio_clock_out),
+    .s_axi_aresetn   (~wrapperio_reset_out),
+    .s_axi_arready   (wrapperio_m_axi_arready),
+    .s_axi_rdata     (wrapperio_m_axi_rdata),
+    .s_axi_rlast     (wrapperio_m_axi_rlast),
+    .s_axi_rresp     (wrapperio_m_axi_rresp),
+    .s_axi_rvalid    (wrapperio_m_axi_rvalid),
+    .s_axi_awready   (wrapperio_m_axi_awready),
+    .s_axi_bresp     (wrapperio_m_axi_bresp),
+    .s_axi_bvalid    (wrapperio_m_axi_bvalid),
+    .s_axi_wready    (wrapperio_m_axi_wready),
+    .s_axi_araddr    (wrapperio_m_axi_araddr),
+    .s_axi_arburst   (wrapperio_m_axi_arburst),
+    .s_axi_arcache   (wrapperio_m_axi_arcache),
+    .s_axi_arlen     (wrapperio_m_axi_arlen),
+    .s_axi_arprot    (wrapperio_m_axi_arprot),
+    .s_axi_arsize    (wrapperio_m_axi_arsize),
+    .s_axi_arvalid   (wrapperio_m_axi_arvalid),
+    .s_axi_rready    (wrapperio_m_axi_rready),
+    .s_axi_awaddr    (wrapperio_m_axi_awaddr),
+    .s_axi_awburst   (wrapperio_m_axi_awburst),
+    .s_axi_awcache   (wrapperio_m_axi_awcache),
+    .s_axi_awlen     (wrapperio_m_axi_awlen),
+    .s_axi_awprot    (wrapperio_m_axi_awprot),
+    .s_axi_awsize    (wrapperio_m_axi_awsize),
+    .s_axi_awvalid   (wrapperio_m_axi_awvalid),
+    .s_axi_bready    (wrapperio_m_axi_bready),
+    .s_axi_wdata     (wrapperio_m_axi_wdata),
+    .s_axi_wlast     (wrapperio_m_axi_wlast),
+    .s_axi_wstrb     (wrapperio_m_axi_wstrb),
+    .s_axi_wvalid    (wrapperio_m_axi_wvalid),
+    .s_axi_awlock    (0),
+    .s_axi_arlock    (0),
+    .bram_rst_a      (bram_rst),
+    .bram_clk_a      (bram_clk),
+    .bram_en_a       (bram_en),
+    .bram_we_a       (bram_we),
+    .bram_addr_a     (bram_addr),
+    .bram_wrdata_a   (bram_wrdata),
+    .bram_rddata_a   (bram_rddata)
+    );
+ 
+    blk_mem_gen_0 bram(
+    .clka            (bram_clk),
+    .ena             (bram_en),
+    .addra           (bram_addr), 
+    .wea             (bram_we),
+    .dina            (bram_wrdata),
+    .douta           (bram_rddata)
+    );
+    
 endmodule
 
 
