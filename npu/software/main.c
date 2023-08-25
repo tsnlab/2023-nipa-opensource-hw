a2c718e29ebebdb10f5dfd6502b4eb4cf493a566
Seungwoo Lee <lsw8075@gmail.com>
2023-08-25 16:00:41 +0900

    RoCC 4 operation

--- npu/software/main.c
+++ npu/software/main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include "bfloat16.h"
 
 #define STR1(x) #x
 #ifndef STR
@@ -17,12 +18,79 @@
   }
 
 
-#define NPU_CUSTOM(rd, rs1, rs2) ROCC_INSTRUCTION_R_R_R(3, rd, rs1, rs2, 0)
+#define ROCC_INSTRUCTION_0_R_R(x, rs1, rs2, func3, func7)                                   \
+  {                                                                                  \
+    asm volatile(                                                                    \
+        ".insn r " STR(CAT(CUSTOM_, x)) ", " STR(func3) ", " STR(func7) ", x0, %0, %1" \
+        :                                                                            \
+        : "r"(rs1), "r"(rs2));                                                       \
+  }
+
+
+#define NPU_LOAD_F16(count, sram, dram) ROCC_INSTRUCTION_0_R_R(3, (count) | ((sram) << 16), dram, 0x3, 2)
+#define NPU_STORE_F16(count, sram, dram) ROCC_INSTRUCTION_0_R_R(3, (count) | ((sram) << 16), dram, 0x3, 3)
+#define NPU_ADD_F16(count, addrC, addrA, addrB) ROCC_INSTRUCTION_0_R_R(3, (count) | ((addrC) << 16), (addrA) | ((addrB) << 16), 0x3, 4)
+#define NPU_SUB_F16(count, addrC, addrA, addrB) ROCC_INSTRUCTION_0_R_R(3, (count) | ((addrC) << 16), (addrA) | ((addrB) << 16), 0x3, 5)
+#define NPU_MUL_F16(count, addrC, addrA, addrB) ROCC_INSTRUCTION_0_R_R(3, (count) | ((addrC) << 16), (addrA) | ((addrB) << 16), 0x3, 6)
+#define NPU_DIV_F16(count, addrC, addrA, addrB) ROCC_INSTRUCTION_0_R_R(3, (count) | ((addrC) << 16), (addrA) | ((addrB) << 16), 0x3, 7)
+
+#define NPU_ADD(a, b, c) ROCC_INSTRUCTION_R_R_R(3, c, a, b, 0)
+#define NPU_SUB(a, b, c) ROCC_INSTRUCTION_R_R_R(3, c, a, b, 1)
+#define NPU_MUL(a, b, c) ROCC_INSTRUCTION_R_R_R(3, c, a, b, 2)
+#define NPU_DIV(a, b, c) ROCC_INSTRUCTION_R_R_R(3, c, a, b, 3)
+
+static uint64_t read_cycles() {
+    uint64_t cycles;
+    asm volatile ("rdcycle %0" : "=r" (cycles));
+    return cycles;
+}
 
 int main(void) {
-    int x = 100, y = 200, z;
-    printf("100 + 200 = ");
-    NPU_CUSTOM(z, x, y);
-    printf("%d\n", z);
-    return 0;
+	int i;
+	uint64_t A[4], B[4], Radd[4], Rsub[4], Rmul[4], Rdiv[4];
+	A[0] = f32_to_bf16(1.0); A[1] = f32_to_bf16(3.0); A[2] = f32_to_bf16(5.0); A[3] = f32_to_bf16(7.0);
+	B[0] = f32_to_bf16(4.0); B[1] = f32_to_bf16(3.0); B[2] = f32_to_bf16(2.0); B[3] = f32_to_bf16(1.0);
+	printf("A: "); for (i=0; i<4; i++) printf("%x ", A[i]); puts("");
+	printf("B: "); for (i=0; i<4; i++) printf("%x ", B[i]); puts("");
+	printf("fpu-add: "); for (i=0; i<4; i++) printf("%x ", bf16_add(A[i], B[i])); puts("");
+	printf("fpu-sub: "); for (i=0; i<4; i++) printf("%x ", bf16_add(A[i], bf16_neg(B[i]))); puts("");
+	printf("fpu-mul: "); for (i=0; i<4; i++) printf("%x ", bf16_mul(A[i], B[i])); puts("");
+	printf("fpu-div: "); printf("%x %x %x %x\n",
+		(int)f32_to_bf16(0.25), (int)f32_to_bf16(1.0),
+		(int)f32_to_bf16(2.5), (int)f32_to_bf16(7.0));
+NPU_ADD(A[1], B[1], Radd[1]);
+	uint32_t start = read_cycles();
+
+	for (i=0; i<4; i++) {
+		NPU_ADD(A[i], B[i], Radd[i]);
+		NPU_SUB(A[i], B[i], Rsub[i]);
+		NPU_MUL(A[i], B[i], Rmul[i]);
+		NPU_DIV(A[i], B[i], Rdiv[i]);
+	}
+	
+/*	
+	NPU_LOAD_F16(4, 0x0, A);
+	NPU_LOAD_F16(4, 0x10, B); 
+
+	NPU_ADD_F16(4, 0x100, 0x0, 0x10);
+	NPU_STORE_F16(4, 0x100, Radd);
+
+	NPU_SUB_F16(4, 0x110, 0x0, 0x10);
+	NPU_STORE_F16(4, 0x110, Rsub);
+
+	NPU_MUL_F16(4, 0x120, 0x0, 0x10);
+	NPU_STORE_F16(4, 0x120, Rmul);
+	NPU_DIV_F16(4, 0x130, 0x0, 0x10);
+	NPU_STORE_F16(4, 0x130, Rdiv);
+*/
+    asm volatile("fence.i");
+
+	uint32_t end = read_cycles();
+    printf("Cycles taken: %lu\n", end-start);
+
+	printf("npu-add: "); for (i=0; i<4; i++) printf("%x ", Radd[i]); puts("");
+	printf("npu-sub: "); for (i=0; i<4; i++) printf("%x ", Rsub[i]); puts("");
+	printf("npu-mul: "); for (i=0; i<4; i++) printf("%x ", Rmul[i]); puts("");
+	printf("npu-div: "); for (i=0; i<4; i++) printf("%x ", Rdiv[i]); puts("");
+	return 0;
 }
--- npu/src/main/scala/RoCC.scala
+++ npu/src/main/scala/RoCC.scala
@@ -4,6 +4,7 @@ import chisel3.util._
 import org.chipsalliance.cde.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tile._
+import hardfloat._
 
 class NPU(implicit p: Parameters) extends LazyRoCC(opcodes = OpcodeSet.custom3) {
   override lazy val module = new NPUModule(this)
@@ -17,11 +18,39 @@ class NPUModule(outer: NPU) extends LazyRoCCModuleImp(outer) with HasCoreParamet
   val data2 = cmd.bits.rs2
 
   val doAdd = (funct === 0.U)
+  val doSub = (funct === 1.U)
+  val doMul = (funct === 2.U)
+  val doDiv = (funct === 3.U)
+
+  val rec1 = recFNFromFN(8, 8, data1(15, 0))
+  val rec2 = recFNFromFN(8, 8, data2(15, 0))
+  val neg_rec2 = recFNFromFN(8, 8, Cat(~data2(15), data2(14, 0)))
+  val one_rec = recFNFromFN(8, 8, "h3F80".U)
+
+  val muladd_rec = Module(new MulAddRecFN(8, 8))
+  val div_rec = Module(new DivSqrtRecFN_small(8, 8, 0))
+
+  muladd_rec.io.op := 0.U
+  muladd_rec.io.a := rec1
+  muladd_rec.io.b := Mux(doMul, rec2, one_rec)
+  muladd_rec.io.c := Mux(doSub, neg_rec2, Mux(doAdd, rec2, 0.U))
+  muladd_rec.io.roundingMode := consts.round_near_even
+  muladd_rec.io.detectTininess := consts.tininess_afterRounding
+
+  div_rec.io.inValid := cmd.valid && doDiv
+  div_rec.io.sqrtOp := false.B
+  div_rec.io.a := rec1
+  div_rec.io.b := rec2
+  div_rec.io.roundingMode := consts.round_near_even
+  div_rec.io.detectTininess := consts.tininess_afterRounding
+
+  val muladd_result = fNFromRecFN(8, 8, muladd_rec.io.out)
+  val div_result = fNFromRecFN(8, 8, div_rec.io.out)
 
   cmd.ready := io.resp.ready
-  io.resp.valid := cmd.valid
+  io.resp.valid := (cmd.valid && !doDiv) || div_rec.io.outValid_div
   io.resp.bits.rd := cmd.bits.inst.rd
-  io.resp.bits.data := data1 + data2
+  io.resp.bits.data := Mux(div_rec.io.outValid_div, div_result, muladd_result)
 
   io.busy := cmd.valid
   io.interrupt := false.B
